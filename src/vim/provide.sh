c93e6206b7dab0451350b97f64d51285cae47dbc
AHaliq <liqo12@hotmail.com>
2017-08-26 15:03:38 +0800

    pathogen base

--- src/vim/provide.sh
+++ src/vim/provide.sh
@@ -2,5 +2,10 @@ echo "========="
 echo "SETUP VIM"
 echo "========="
 
+# base
 apt-get -y install vim
 cp /vagrant/src/vim/.vimrc ~/
+
+# pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
