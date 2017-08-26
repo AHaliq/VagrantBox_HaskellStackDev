8fd17e6b5eeffd26fcdf3ac78ccd33ee317b651f
AHaliq <liqo12@hotmail.com>
2017-08-26 15:38:54 +0800

    pathogen plugins

--- src/vim/provide.sh
+++ src/vim/provide.sh
@@ -9,3 +9,16 @@ cp /vagrant/src/vim/.vimrc ~/
 # pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Plugins
+cd ~/.vim/bundle
+  # File Management
+git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jreybert/vimagit.git
+  # Text Editing
+git clone git://github.com/godlygeek/tabular.git
+git clone https://github.com/tpope/vim-surround.git
+  # Additional UI
+git clone https://github.com/airblade/vim-gitgutter.git
+git clone https://github.com/majutsushi/tagbar.git
+git clone https://github.com/Twinside/vim-hoogle.git
